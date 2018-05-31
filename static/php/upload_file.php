667e273f9cc67d6d84137e1770b30ad5f6d4c90f
linxi <lx02321@163.com>
2018-06-01 00:07:30 +0800

    优化上传文件返回状态(全改为了json格式)

--- static/php/upload_file.php
+++ static/php/upload_file.php
@@ -14,39 +14,51 @@ while( ($rootfilename = readdir($handler)) !== false )
         $needpath = $rootfilename;
     }
 }
-echo $_FILES["file"]["size"];
 $extension = end($temp);     // 获取文件后缀名
 if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/jpg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/x-png")
-|| ($_FILES["file"]["type"] == "image/png"))
-&& ($_FILES["file"]["size"] < 204800)   // 小于 200 kb
+    || ($_FILES["file"]["type"] == "image/jpeg")
+    || ($_FILES["file"]["type"] == "image/jpg")
+    || ($_FILES["file"]["type"] == "image/pjpeg")
+    || ($_FILES["file"]["type"] == "image/x-png")
+    || ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 512000)   // 小于 400 kb
 && in_array($extension, $allowedExts))
 {
     if ($_FILES["file"]["error"] > 0)
     {
-        echo "错误：: " . $_FILES["file"]["error"] . "<br>";
+        echo json_encode(array(
+            "result"=>"200",
+            "message"=>"上传成功!"
+        ));
     }
     else
     {
-        echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
-        echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
-        echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-        echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"] . "<br>";
-        
+        //echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
+        //echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
+        //echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+        //echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"] . "<br>";
+
         // 判断当期目录下的 upload 目录是否存在该文件
         // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
         if (file_exists("upload/" . $_FILES["file"]["name"]))
         {
-            echo $_FILES["file"]["name"] . " 文件已经存在。 ";
+            //echo $_FILES["file"]["name"] . " 文件已经存在。 ";
         }
         else
         {
             // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-            move_uploaded_file($_FILES["file"]["tmp_name"], $root_dir_name."/axi/".$needpath."/" . $_FILES["file"]["name"]);
-            echo "文件存储在: " . $root_dir_name."/axi/".$needpath;
+            if(move_uploaded_file($_FILES["file"]["tmp_name"], $root_dir_name."/axi/".$needpath."/" . $_FILES["file"]["name"])){
+                echo json_encode(array(
+                    "result"=>"200",
+                    "message"=>"上传成功!"
+                ));
+            }else {
+                echo json_decode(array(
+                    "result"=>"201",
+                    "message"=>"上传失败!"
+                ));
+            }
+
             //echo $_SERVER['DOCUMENT_ROOT'];
         }
     }
