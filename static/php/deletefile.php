c8ccd6eb9f9b5724f7cc83bc0bb5ccc254834071
linxi <lx02321@163.com>
2018-06-01 00:05:59 +0800

    添加跨域资源共享请求头

--- static/php/deletefile.php
+++ static/php/deletefile.php
@@ -1,6 +1,8 @@
 <?php 
+header('Access-Control-Allow-Origin:https://localhost:8080');
+header('Access-Control-Allow-Origin:*');
 $filename = $_POST["filename"];
-$path = $_SERVER['DOCUMENT_ROOT']."/axi/img_1.0.3/";
+$path = $_SERVER['DOCUMENT_ROOT']."/axi/img_1.0.0/";
 $filepath = $path.$filename;
 if(is_file($filepath)){
 	if(unlink($filepath)){
